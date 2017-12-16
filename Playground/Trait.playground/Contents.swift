5d1ee928771b22c3e2b435002f1b018598a7d5c1
shoheiyokoyama <shohei.yok0602@gmail.com>
2017-12-16 19:19:09 +0900

    Add RxSwift traits

--- Playground/Trait.playground/Contents.swift
+++ Playground/Trait.playground/Contents.swift
@@ -2,5 +2,95 @@
 
 import UIKit
 import RxSwift
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let disposeBag = DisposeBag()
+
+func getDataOrError(completionHandler: @escaping ((String?, Error?)) -> Void) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
+//        return completionHandler((nil, NSError()))
+        return completionHandler(("data fetched!", nil))
+    }
+}
+
+var singleObservable: Single<String> {
+    return Single.create { single in
+        getDataOrError { (data, error) in
+            if let error = error {
+                single(.error(error))
+                return
+            }
+            
+            single(.success(data!))
+        }
+        return Disposables.create()
+    }
+}
+
+singleObservable
+    .subscribe({
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+enum Result {
+    case success
+    case error(Error)
+}
+
+func excuteWork(completionHandler: @escaping (Result) -> Void) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
+//        return completionHandler(.error(NSError()))
+        return completionHandler(.success)
+    }
+}
+
+var completableObservable: Completable {
+    return Completable.create { completable in
+        excuteWork { result in
+            if case let .error(e) = result {
+                completable(.error(e))
+            }
+            completable(.completed)
+        }
+
+        return Disposables.create {}
+    }
+}
+
+completableObservable
+    .subscribe({
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+var maybeObservable: Maybe<String> {
+    return Maybe<String>.create { maybe in
+        maybe(.success("Maybe"))
+
+        // OR
+//        maybe(.completed)
+
+        // OR
+//        maybe(.error(error))
+
+        return Disposables.create {}
+    }
+}
+
+maybeObservable
+    .subscribe({
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+// same as `maybeObservable`
+Observable.just(1).asMaybe()
+    .subscribe({
+        print($0)
+    })
+    .disposed(by: disposeBag)
 
 
--- RxTraitSample/ViewController.swift
+++ RxTraitSample/ViewController.swift
@@ -7,12 +7,49 @@
 //
 
 import UIKit
+import RxSwift
+
+let disposeBag = DisposeBag()
+enum Result<V> {
+    case success(V)
+    case error(Error)
+}
+
+let url = URL(string: "http://lorempixel.com/400/200/")!
+
+var singleObservable: Single<Result<UIImage>> {
+    return Single.create { single in
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                single(.error(error))
+                single(.success(Result.success(UIImage())))
+                return
+            }
+            
+            guard let data = data,
+                let image = UIImage(data: data) else {
+                    single(.success(Result.error(NSError())))
+                    return
+            }
+            single(.success(Result.error(NSError(domain: "test", code: 2, userInfo: [:]))))
+            single(.success(Result.success(image)))
+        }
+        
+        task.resume()
+        
+        return Disposables.create { task.cancel() }
+    }
+}
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        singleObservable
+            .debug("", trimOutput: false)
+            .subscribe({
+                    print("o!!!!!!!!!!!!!!!!!!!!!!: \($0)")
+            })
+            .disposed(by: disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
